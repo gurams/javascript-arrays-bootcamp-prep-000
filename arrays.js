6679f0227b7c97e13df79fe360c6aa4fbcbef800
Ramesh <gurams@gmail.com>
2017-10-21 20:23:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,21 +2,23 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 var ele ="foo"
 function addElementToBeginningOfArray(chocolateBars,ele) {
-  return chocolateBars.unshift(ele)
-}
-
-function destructivelyAddElementToBeginningOfArray(chocolateBars,ele){
   return (ele,...chocolateBars)
 }
 
-function addElementToEndOfArray (chocolateBars,ele){
-  return chocolateBars.push(ele)
+function destructivelyAddElementToBeginningOfArray(chocolateBars,ele){
+  chocolateBars.unshift(ele)
+  return chocolateBars
 }
 
-function destructivelyAddElementToEndOfArray(chocolateBars,ele){
+function addElementToEndOfArray (chocolateBars,ele){
   return (...chocolateBars,ele)
 }
 
-function accessElementInArray(chocolateBars,2){
-  return chocolateBars(3)
+function destructivelyAddElementToEndOfArray(chocolateBars,ele){
+  chocolateBars.push(ele)
+  return chocolateBars
+}
+
+function accessElementInArray(chocolateBars,index){
+  return chocolateBars[index]
 }
